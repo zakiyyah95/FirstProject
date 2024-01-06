6d50141d6451c42c6ead6e0adf673f8aa44fd562
zakiyyah95 <zakiyyah.be@gmail.com>
2024-01-06 12:13:27 -0500

    Initial Commit.

---
+++ src/com/smart/tests/Test.java
@@ -0,0 +1,13 @@
+package com.smart.tests;
+
+public class Test {
+
+	@org.testng.annotations.Test
+	public void loginTest() {
+		
+		System.out.println("We are learning GitHub through Eclipse.");
+		
+	}
+	
+	
+}
