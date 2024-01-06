aa6eb591e06303c61e2a1c190433d17f49bb22d7
zakiyyah95 <90148595+zakiyyah95@users.noreply.github.com>
2024-01-06 13:02:00 -0500

    Update Test.java

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	@org.testng.annotations.Test
 	public void loginTest() {
-		
+		//Adding a comment 
 		System.out.println("We are learning GitHub through Eclipse.");
 		System.out.println("We had our initial commit.");
 		
